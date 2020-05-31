5088a31b8b4b3df012589035d6a6a873e27b0fb2
hgupta1992 <hemantgupta4159@gmail.com>
2020-05-31 10:18:56 +0000

    Lab5 and 6

--- lab05.main.js
+++ lab05.main.js
@@ -80,14 +80,7 @@ class ServiceNowAdapter extends EventEmitter {
   connect() {
     // As a best practice, Itential recommends isolating the health check action
     // in its own method.
-    this.healthcheck((result, error) => {
-      if (error) {
-        console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
-      }
-      else {
-        console.log(`\nResponse returned from GET request:\n${JSON.stringify(result)}`)
-      }
-    });
+    this.healthcheck();
   }
 
   /**
@@ -101,46 +94,9 @@ class ServiceNowAdapter extends EventEmitter {
    *   that handles the response.
    */
   healthcheck(callback) {
-    this.getRecord((result, error) => {
-      /**
-       * For this lab, complete the if else conditional
-       * statements that check if an error exists
-       * or the instance was hibernating. You must write
-       * the blocks for each branch.
-       */
-
-      if (error) {
-        /**
-         * Write this block.
-         * If an error was returned, we need to emit OFFLINE.
-         * Log the returned error using IAP's global log object
-         * at an error severity. In the log message, record
-         * this.id so an administrator will know which ServiceNow
-         * adapter instance wrote the log message in case more
-         * than one instance is configured.
-         * If an optional IAP callback function was passed to
-         * healthcheck(), execute it passing the error seen as an argument
-         * for the callback's errorMessage parameter.
-         */
-        this.emitOffline();
-        return callback(null, error);
-
-      } else {
-        /**
-         * Write this block.
-         * If no runtime problems were detected, emit ONLINE.
-         * Log an appropriate message using IAP's global log object
-         * at a debug severity.
-         * If an optional IAP callback function was passed to
-         * healthcheck(), execute it passing this function's result
-         * parameter as an argument for the callback function's
-         * responseData parameter.
-         */
-        this.emitOnline();
-        return callback(result, null);
-
-      }
-    });
+    // We will build this method in a later lab. For now, it will emulate
+    // a healthy integration by emmitting ONLINE.
+    this.emitOnline();
   }
 
   /**
@@ -196,7 +152,6 @@ class ServiceNowAdapter extends EventEmitter {
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-    this.connector.get((results, error) => callback(results, error));
   }
 
   /**
@@ -215,7 +170,6 @@ class ServiceNowAdapter extends EventEmitter {
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
-    this.connector.post((results, error) => callback(results, error));
   }
 }
 
